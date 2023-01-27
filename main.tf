4b1c5966a8462d7b85c27a7027eeef3aa412379c
kyeongju <ainimo2@gmail.com>
2023-01-27 12:01:44 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "this" {
   instance_type        = var.instance_type
   cpu_core_count       = var.cpu_core_count
   cpu_threads_per_core = var.cpu_threads_per_core
-  hibernation          = var.hibernation
+  hibernation          = try(var.hibernation, false)
 
   user_data                   = var.user_data
   user_data_base64            = var.user_data_base64
@@ -172,7 +172,7 @@ resource "aws_spot_instance_request" "this" {
   instance_type        = var.instance_type
   cpu_core_count       = var.cpu_core_count
   cpu_threads_per_core = var.cpu_threads_per_core
-  hibernation          = var.hibernation
+  hibernation          = try(var.hibernation, false)
 
   user_data                   = var.user_data
   user_data_base64            = var.user_data_base64
